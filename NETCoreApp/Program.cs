dbba74c2aa49f87fb9a475d14c96d496c19cd337
Shi Hao Wang <st890409@hotmail.com>
2021-04-24 16:33:51 +0800

    Add test console app

---
+++ NETCoreApp/Program.cs
@@ -0,0 +1,79 @@
+﻿using SequentialGuid;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace NETCoreApp
+{
+    class Program
+    {
+        const string rootPath = "../../../files";
+
+        static void Main(string[] args)
+        {
+            List<Task> _tasks;
+
+            _tasks = new List<Task>();
+
+            var _files = Directory.GetFiles(rootPath, "*.guid");
+
+            //Dictionary not allow duplicate key
+            var _dictionary = new Dictionary<string, string>();
+
+            foreach (var _filePath in _files)
+                File.Delete(_filePath);
+
+            for (int i = 0; i < 50; i++)
+            {
+                Task _task = new Task(() => {
+                    var _fileName = $"{Guid.NewGuid()}.guid";
+                    var _fullPath = Path.Combine(rootPath, _fileName);
+
+                    for (int i = 0; i < 2500; i++)
+                    {
+                        var _guidValue = SequentialGuidGenerator.Instance.NewGuid();
+                        var _content = $"{_guidValue}\n";
+
+                        File.AppendAllText(_fullPath, _content);
+                    }
+                });
+                _tasks.Add(_task);
+                _task.Start();
+            }
+
+            Task.WaitAll(_tasks.ToArray());
+
+            _files = Directory.GetFiles(rootPath, "*.guid");
+
+            foreach (var _filePath in _files)
+            {
+                using(var _reader = new StreamReader(_filePath))
+                {
+                    while(true)
+                    {
+                        if (_reader.Peek() == -1)
+                            break;
+
+                        var _value = _reader.ReadLine();
+
+                        if (string.IsNullOrWhiteSpace(_value))
+                            break;
+
+                        _dictionary.Add(_value, _value);
+                    }
+                }
+            }
+
+            foreach (var _item in _dictionary)
+                Console.WriteLine(_item.Key);
+
+            Console.WriteLine($"{_dictionary.Count} SequentialGuids Created");
+            Console.WriteLine("Not duplicate Guid found !");
+            Console.WriteLine();
+
+            Console.WriteLine("press any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
